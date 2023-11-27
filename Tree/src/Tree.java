d37deb64fc0c69f307a07b881577577ea5e7ab6f
George <gnjuki19@gmail.com>
2023-11-27 11:47:17 +0300

    Leaning to write Java code

---
+++ Tree/src/Tree.java
@@ -0,0 +1,19 @@
+
+public class Tree {
+    public static void main(String[] args){
+        // Introducing myself and plant a tree
+        String greeting = "Hey there, I’m George and I’m learning to code in Java!";
+
+        System.out.println(greeting);
+
+        System.out.println("Ready to get my hands dirty!");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+
+    }
+}
