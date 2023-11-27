bfb0f24526e30622341226f0743567b1e90030a9
George <gnjuki19@gmail.com>
2023-11-27 11:52:19 +0300

    Working with variables

---
+++ MyProfile/src/MyProfile.java
@@ -0,0 +1,13 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "George";
+        int age = 32;
+        double desiredSalary = 100000.00;
+        char gender = 'm';
+        boolean lookingForJob = true;
+
+        System.out.println("My name is " + name + " and I'm " + age + " years old.");
+    }
+
+
+}
