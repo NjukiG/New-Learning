578deb359828a5aa08d5d4c007ca400257ca9577
George <gnjuki19@gmail.com>
2023-12-02 22:50:09 +0300

    Practiced user more interaction

--- User-Interaction2/src/Student.java
+++ User-Interaction2/src/Student.java
@@ -13,15 +13,10 @@ public class Student {
         this.name = name;
     }
 
-    /**
-     * Take in a student numeric grade and provide feedback
-     * on how they did on a test
-     * @param grade : int - numeric grade the student received
-     *                on the test
-     */
+
     public void calculateGrade(int grade) {
         if (grade >= 70) {
-            System.out.println("Congrats! You passed!");
+            System.out.println("Congrats, " + name + "! You passed!");
             if (grade >= 90) {
                 System.out.println("Wow! You got an A!");
             }
